66a012ba961826b1f48528e341f58440247c4fa4
ayanakkabi1 <aynour200609@gmail.com>
2026-01-27 15:02:59 +0100

    REC-1 feat: implement create and store methods in RecipeController for recipe management

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,26 @@
 namespace App\Http\Controllers;
 use App\Models\Recipe;
 use Illuminate\View\View;
+use Illuminate\Http\Request;
+
 class RecipeController extends Controller {
     public function index(){
         $recipes=Recipe::all();
         return view('recipes.index',compact('recipes'));
+    }
+    public function create(): View {
+        return view('recipes.create');
+    }
+    public function store(Request $request) {
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string',
+            'ingredients' => 'required|string',
+            'steps' => 'required|string',
+            'image' => 'required|string|max:255',
+            'category' => 'required|string|max:255',
+        ]);
+        Recipe::create($validatedData);
+        return redirect()->route('recipes.index')->with('success', 'Recipe created successfully.');
     }
 }
