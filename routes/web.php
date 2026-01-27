8b97c2b8f24ae82adb381279f32dcfe120656de9
ayanakkabi1 <aynour200609@gmail.com>
2026-01-27 16:22:01 +0100

    REC - 2 fix(routes): restore and properly define recipe routes with naming conventions

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RecipeController;
 
+// Accueil
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-});
-Route::get('/recipes', [RecipeController::class, 'index']);
+
+Route::get('/recipes', [RecipeController::class, 'index'])->name('recipes.index');
+Route::get('/recipes/create', [RecipeController::class, 'create'])->name('recipes.create');
 Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store');
-Route::get('/recipes', [RecipeController::class, 'create'])->name('recipes.index');
+Route::get('/recipes/{id}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
+Route::put('/recipes/{id}', [RecipeController::class, 'update'])->name('recipes.update');
+Route::delete('/recipes/{id}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
