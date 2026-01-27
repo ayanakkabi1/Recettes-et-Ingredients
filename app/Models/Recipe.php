743034b1dacd95ab30d76429cf43be62f6e8fa97
ayanakkabi1 <aynour200609@gmail.com>
2026-01-27 15:05:31 +0100

    REC-1 feat: define fillable properties in Recipe model for mass assignment

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Recipe extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = [
+        'name',
+        'description',
+        'ingredients',
+        'steps',
+        'image',
+        'category'
+    ];
 }
