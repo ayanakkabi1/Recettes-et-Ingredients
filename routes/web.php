b493200e1a61b7d52cb2c510f5ac06aa8aa0d6fe
ayanakkabi1 <aynour200609@gmail.com>
2026-01-27 23:08:01 +0100

    REC-5 fix(routes): remove duplicate route definition for recipe index

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::post('/recipes', [RecipeController::class, 'store'])->name('recipes.store
 Route::get('/recipes/{id}/edit', [RecipeController::class, 'edit'])->name('recipes.edit');
 Route::put('/recipes/{id}', [RecipeController::class, 'update'])->name('recipes.update');
 Route::delete('/recipes/{id}', [RecipeController::class, 'destroy'])->name('recipes.destroy');
+Route::get('/recipes', [App\Http\Controllers\RecipeController::class, 'index'])->name('recipes.index');
