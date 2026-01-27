2ddff8b3d9e0418b1ed43fafebc610b7fb75b3c9
ayanakkabi1 <aynour200609@gmail.com>
2026-01-27 10:14:45 +0100

    feat: add database connection and recipe migration

---
+++ database/migrations/2026_01_27_090351_create_recipes_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->text('ingredients');
+            $table->text('steps');
+            $table->string('image');
+            $table->string('category');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
